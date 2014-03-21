52a6db4e58a08d24ce5da5589b8ffdd797af9d2c
azu <azuciao@gmail.com>
2014-03-21 22:29:16 +0900

    feat(find): improve find logic

--- intelli-espower-loader.js
+++ intelli-espower-loader.js
@@ -1,8 +1,25 @@
 "use strict";
 var assert = require("assert");
+var fs = require("fs");
+var pather = require("path");
+var packageName = require("./package.json").name;
+
+function findPackageDir(paths) {
+    if (!paths) {
+        return null;
+    }
+    for (var i = 0; i < paths.length; ++i) {
+        var dir = pather.dirname(paths[i]);
+        var dirName = dir.split(pather.sep).pop();
+        if (dirName !== packageName && fs.existsSync(pather.join(dir, 'package.json'))) {
+            return dir;
+        }
+    }
+}
 function getPackageJSON() {
-    var pather = require("path");
-    return require(pather.resolve(__dirname, "../../", "package.json"));
+    var dir = findPackageDir(module.paths);
+    assert(dir, "package.json is not found");
+    return require(pather.resolve(dir, "package.json"));
 }
 var directories = getPackageJSON().directories;
 assert.equal(typeof directories, "object", 'You should setting `directories : { test : "test/" }`');
