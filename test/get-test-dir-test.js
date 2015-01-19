79f626f8f41cda7f9c6f72ee2f2b767c853b3b52
azu <azuciao@gmail.com>
2015-01-19 14:40:22 +0900

    feat(intelli-espower-loader): add default patterns

---
+++ test/get-test-dir-test.js
@@ -0,0 +1,24 @@
+// LICENSE : MIT
+"use strict";
+var assert = require("assert");
+var getTestDir = require("../lib/get-test-dir");
+describe("get-test-dir-test", function () {
+    context("when `pkg.directories.test` is not found", function () {
+        it("should return 'test/'", function () {
+            var dir = getTestDir({
+                "directories": {}
+            });
+            assert.equal(dir, "test/");
+        });
+    });
+    context("when `pkg.directories.test` is set", function () {
+        it("should return 'test/'", function () {
+            var dir = getTestDir({
+                "directories": {
+                    "test": "user-dir"
+                }
+            });
+            assert.equal(dir, "user-dir/");
+        });
+    });
+});
